ce8b7b66c1cb44b18cd8c06138b343a063a10139
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-06-23 13:55:04 +0530

    Remove trailing slash from CORS origin URL

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "https://interview-iq-self.vercel.app/",
+    origin: "https://interview-iq-self.vercel.app",
     credentials: true
 }))
 
