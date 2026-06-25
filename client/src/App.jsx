770659d773a19bbcb32dd35119d02aa465984ae9
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-06-25 16:15:41 +0530

    serverURL in .env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from "./pages/InterviewHistory";
 import Pricing from "./pages/Pricing";
 import InterviewReport from "./pages/InterviewReport";
 
-export const serverURL = "https://interviewiq-h6lw.onrender.com";
+export const serverURL = import.meta.env.VITE_SERVER_URL;
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
